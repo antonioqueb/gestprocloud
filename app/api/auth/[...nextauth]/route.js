8590736946009e93d51c0dfc0a40d2d9a6d27d58
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-06-01 14:28:08 -0600

    feat: update import statements in auth route
    The import statements in the auth route have been updated to use double quotes instead of single quotes. This change ensures consistency in the codebase and follows the established convention of using double quotes for import statements.

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
-import NextAuth from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import { PrismaClient } from '@prisma/client';
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
@@ -15,52 +15,33 @@ export const authOptions = {
   ],
   callbacks: {
     async jwt({ token, account, user }) {
+      console.log('JWT Callback - Initial', { token, account, user });
       if (account) {
         token.accessToken = account.access_token;
-        token.id = user.id;
+        token.id = user.id; // Asigna el ID del usuario al token
+        console.log('Access Token and User ID Assigned in JWT Callback', token.accessToken, token.id);
       }
+      console.log('JWT Callback - Final', token);
       return token;
     },
-    async session({ session, token }) {
+    async session({ session, token, user }) {
+      console.log('Session Callback - Initial', { session, token, user });
       if (token) {
         session.accessToken = token.accessToken;
-        session.user.id = token.id;
+        session.user.id = token.id; // Asegúrate de asignar el ID del usuario a la sesión
+        console.log('Session Access Token and User ID Assigned', session.accessToken, session.user.id);
       }
+      console.log('Session Callback - Final', session);
       return session;
     },
   },
-  cookies: {
-    sessionToken: {
-      name: `next-auth.session-token`,
-      options: {
-        httpOnly: true,
-        sameSite: 'lax',
-        path: '/',
-        secure: process.env.NODE_ENV === 'production',
-      },
-    },
-    callbackUrl: {
-      name: `next-auth.callback-url`,
-      options: {
-        sameSite: 'lax',
-        path: '/',
-        secure: process.env.NODE_ENV === 'production',
-      },
-    },
-    csrfToken: {
-      name: `next-auth.csrf-token`,
-      options: {
-        httpOnly: true,
-        sameSite: 'lax',
-        path: '/',
-        secure: process.env.NODE_ENV === 'production',
-      },
-    },
-  },
-  secret: process.env.NEXTAUTH_SECRET,
   debug: true,
 };
 
-export default async function auth(req, res) {
+export async function GET(req, res) {
+  return NextAuth(req, res, authOptions);
+}
+
+export async function POST(req, res) {
   return NextAuth(req, res, authOptions);
 }
